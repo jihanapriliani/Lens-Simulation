0077aa21ed7a423f6e652082438f4259f7eacd0f
jihanapriliani <11211046@student.itk.ac.id>
2023-03-27 01:59:16 +0800

    adding object in mirror

--- src/components/ConcaveMirror.js
+++ src/components/ConcaveMirror.js
@@ -5,7 +5,7 @@ import Sketch from 'react-p5';
 import { dda, getBoundPoint, drawEllipse, getDistanceReflection, getSizeReflection, setLensLabel, setMarginalConvexMirror } from '../utils';
 
 
-export default function ConvexMirror(props) {
+export default function ConcaveMirror(props) {
     const {width, height, size, distance, focus, hasLabel, ray} = props;
 
     const [distance_, setDistance_] = useState(getDistanceReflection(distance, focus));
@@ -21,12 +21,12 @@ export default function ConvexMirror(props) {
         p5.background(255,255,255);
         
         p5.stroke(32, 111, 153);
+        p5.stroke(32, 111, 153);
         p5.fill(173, 216, 230);
         p5.ellipse(width / 2, height/2, focus, height);
         p5.fill(255,255,255);
         p5.stroke(255);
-        p5.rect(width / 2 - focus/2, 0, focus / 2, height);
-
+        p5.rect(width / 2 + focus / 20, 0, focus / 2, height);
 
         p5.fill(0,0,0);
         p5.textSize(18);
@@ -43,18 +43,18 @@ export default function ConvexMirror(props) {
         p5.stroke(50, 168, 82);
         dda(width / 2 - distance, height / 2, width / 2 - distance, height / 2 - size, p5);
         p5.stroke(85, 115, 70);
-        dda(width / 2 - distance_, height / 2, width / 2 - distance_, height / 2 + size_, p5);
+        dda(width / 2 + distance_, height / 2, width / 2 + distance_, height / 2 - size_, p5);
         
         if(ray === "marginal") {
-            p5.stroke("red");
-            dda(width / 2 - distance, height / 2 - size, width / 2, 0, p5);
-            dda(width / 2, 0, width / 2 - distance_, height / 2 + size_, p5);
-            p5.stroke("yellow");
-            dda(width / 2 - distance, height / 2 - size, width / 2, height / 2, p5);
-            dda(width / 2, height / 2, width / 2 - distance_, height / 2 + size_, p5);
-            p5.stroke("purple");
-            dda(width / 2 - distance, height / 2 - size, width / 2, height, p5);
-            dda(width / 2, height, width / 2 - distance_, height / 2 + size_, p5);
+            // p5.stroke("red");
+            // dda(width / 2 - distance, height / 2 - size, width / 2, 0, p5);
+            // dda(width / 2, 0, width / 2 - distance_, height / 2 + size_, p5);
+            // p5.stroke("yellow");
+            // dda(width / 2 - distance, height / 2 - size, width / 2, height / 2, p5);
+            // dda(width / 2, height / 2, width / 2 - distance_, height / 2 + size_, p5);
+            // p5.stroke("purple");
+            // dda(width / 2 - distance, height / 2 - size, width / 2, height, p5);
+            // dda(width / 2, height, width / 2 - distance_, height / 2 + size_, p5);
             // setMarginalConvexMirror(p5, height, width, infinite1);
 
         }
@@ -62,13 +62,16 @@ export default function ConvexMirror(props) {
         if(ray === "principal") {
             p5.stroke("red");
             dda(width / 2 - distance, height / 2 - size, width / 2, height / 2 - size, p5);
-            dda(width / 2, height / 2 - size, width / 2 - distance_, height / 2 + size_, p5);
+            dda(width / 2 + distance_, height / 2 - size_, width / 2, height / 2 - size, p5)
+
             p5.stroke("yellow");
-            dda(width / 2 - distance, height / 2 - size, width / 2, height / 2, p5);
-            dda(width / 2, height / 2, width / 2 - distance_, height / 2 + size_, p5);
+            dda(width / 2 - distance, height / 2 - size, width / 2, height / 2 - size_, p5);
+            dda(width / 2 + distance_, height / 2 - size_, width / 2, height / 2 - size_, p5);
+            
             p5.stroke("purple");
-            dda(width / 2 - distance, height / 2 - size, width / 2, height, p5);
-            dda(width / 2, height, width / 2 - distance_, height / 2 + size_, p5);
+            dda(width / 2 - distance, height / 2 - size, width / 2, height / 2, p5);
+            dda(width / 2 + distance_, height / 2 - size_, width / 2, height / 2, p5);
+           
 
 
         //   setPrincipalRay(p5, height, width, size, size_, infinite1, infinite2, infinite3, infinite4);
@@ -103,7 +106,7 @@ export default function ConvexMirror(props) {
 }
 
 
-ConvexMirror.propType = {
+ConcaveMirror.propType = {
     height: propType.number,
     width: propType.number,
 }
