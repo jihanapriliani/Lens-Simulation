7d175a589ad9debffa6766dfd6a8b1b39c968513
Djibran1103 <11211070@student.itk.ac.id>
2023-03-26 17:40:57 +0800

    Cekung WIP 2

--- src/components/ConcaveGraph.js
+++ src/components/ConcaveGraph.js
@@ -30,6 +30,30 @@ export default function ConcaveGraph(props) {
         p5.stroke(50, 168, 82);
         dda(width / 2 - distance, height / 2, width / 2 - distance, height / 2 - size, p5);
 
+        p5.stroke("brown")
+        dda(width / 2 - distance + 40, height /2, width / 2 - distance + 40, height / 2 - size/2, p5);
+        dda(width / 2 - distance - 40, height /2 , width / 2 - distance - 40, height / 2 - size /2, p5);
+        dda(width / 2 - distance - 40, height / 2 - size/2 , width /2 - distance  + 40, height / 2 - size/2, p5);
+
+        dda(width / 2 - distance + 20, height /2 ,width / 2 - distance + 20, height / 2 - size/16, p5);
+        dda(width / 2 - distance - 20, height /2, width / 2 - distance - 20, height / 2 - size /16, p5);
+        dda(width / 2 - distance - 20, height / 2 - size/16, width /2 - distance  + 20, height / 2 - size/16, p5);
+
+        dda(width / 2 - distance - 125 , height /2, width / 2 - distance - 150, height / 2 - size  /2 - 17, p5);
+        dda(width / 2 - distance - 100 , height /2, width / 2 - distance - 120, height / 2 - size  /2 - 11, p5);
+        dda(width / 2 - distance - 75 , height /2, width / 2 - distance - 95, height / 2 - size  /2 - 9, p5);
+        dda(width / 2 - distance - 55 , height /2, width / 2 - distance - 75, height / 2 - size  /2 - 5, p5);
+        dda(width / 2 - distance - 40 , height /2, width / 2 - distance - 55, height / 2 - size  /2 - 3, p5);
+        dda(width / 2 - distance - 175, height / 2 - size /2 - 20 , width /2 - distance  -40, height / 2 - size/2, p5);
+
+        dda(width / 2 - distance + 125 , height /2, width / 2 - distance + 150, height / 2 - size  /2 - 17, p5);
+        dda(width / 2 - distance + 100 , height /2, width / 2 - distance + 120, height / 2 -  size  /2 -  11, p5);
+        dda(width / 2 - distance + 75 , height /2, width / 2 - distance + 95, height / 2 - size  /2 -  9, p5);
+        dda(width / 2 - distance + 55 , height /2, width / 2 - distance + 75, height / 2 - size  /2 - 5, p5);
+        dda(width / 2 - distance + 40 , height /2, width / 2 - distance + 55, height / 2 - size  /2 - 3, p5);
+        dda(width / 2 - distance + 175, height / 2 - size /2 - 20, width /2 - distance  +40, height / 2  - size/2, p5);
+        
+
         // REFLECTION: OBJECT
         p5.stroke(85, 115, 70);
         dda(width / 2 - distance, height / 2, width / 2 - distance, height / 2 - size_, p5);
